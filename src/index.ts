b841ab7de30cb8c592f13d7ba496874e7b2e8ff3
BayajidAlam <bayajidalam2001@gmail.com>
2023-12-20 12:53:40 +0600

    query done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { db } from "./db.js";
 
 const typeDefs = `#graphql
   type Product {
@@ -18,23 +19,18 @@ const typeDefs = `#graphql
   }
 `;
 
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
 const resolvers = {
-  Query: {},
+  Query: {
+    products: () => db.products,
+  },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
+
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
