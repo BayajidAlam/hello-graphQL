bab551720aa47d19ce639624ca47b1b3e06aa6b9
BayajidAlam <bayajidalam2001@gmail.com>
2023-12-20 12:46:04 +0600

    schema added

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,26 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type Product {
+    id: ID!,
+    name: String,
+    image: String,
+    description: String,
+    price: Float,
+    quantity: Int,
+    onStock: Boolean,
+    categoryId: String,
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    products: [Product]
   }
 `;
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
-  Query: {
-    books: () => books,
-  },
+  Query: {},
 };
 
 // The ApolloServer constructor requires two parameters: your schema
